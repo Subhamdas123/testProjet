041cc80d3e0f1c825daf42643e78172ab21804af
Subham <sdas69629@gmail.com>
2020-12-08 13:34:52 +0530

    Change 1

---
+++ JavaProgram/src/AlgoQuestions/ReverseAStringWordByWord.java
@@ -0,0 +1,22 @@
+package AlgoQuestions;
+
+public class ReverseAStringWordByWord {
+
+	public static void main(String[] args) {
+		String a="The sky is blue ";
+		String ans="";
+		int i=a.length()-1;
+		while(i>=0) {
+			while(i>=0 && a.charAt(i)==' ')i--;
+		int j=i;
+		while(i>=0 && a.charAt(i)!=' ')i--;
+		if(ans.isEmpty()) {
+			ans=ans.concat(a.substring(i+1,j+1));
+		}else {
+			ans=ans.concat(" "+a.substring(i+1,j+1));
+		}
+		}
+		System.out.println(ans);
+	}
+
+}
